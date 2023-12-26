8cb8b4688224879fb10e9f156a9fc06e404a0928
jinhye <67266348+jinurumi@users.noreply.github.com>
2023-12-27 01:47:15 +0900

    Update train.py

---
+++ train.py
@@ -0,0 +1,11 @@
+from models.ImageGener import StyleGAN
+from models.TTS import tacotron
+
+
+model = StyleGAN()
+model2 = tacotron()
+
+for epoch in epochs:
+    for idx, data in enumerate(dataloader):
+        
+# 학습 완료되면 model.pt 가 저장
